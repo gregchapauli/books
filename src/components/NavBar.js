94456ccf93e8078d90c360da4ddffbc941079dff
Gregory Caloin <gregchapauli@gmail.com>
2022-03-15 14:41:40 +0100

    Nettoyage du projet et création de NavBar et Footer

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <header>
+      <div className="d-flex flex-column flex-md-row p-3 border-bottom bg-secondary text-white">
+        <h4 className="mr-md-auto">BOOKS</h4>
+        <a href="/" className="text-decoration-none text-white">
+          BOOKS
+        </a>
+      </div>
+
+      {/* Menu */}
+    </header>
+  );
+};
+
+export default NavBar;
