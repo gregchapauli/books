ab779798658ce5ffc8e47ef1e1550436adb615ab
Gregory Caloin <gregchapauli@gmail.com>
2022-03-24 14:43:55 +0100

    Mise en place du Fetch de l'API Books de Google

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import reducerAddBooks from "./reducers/reducerAddBooks";
+import reducerFetchedBooks from "./reducers/reducerFetchBooks";
+import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
   library: reducerAddBooks,
+  search: reducerFetchedBooks,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
